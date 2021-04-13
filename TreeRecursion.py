ba678019a23e76817b82fd3706c123764241ff0a
szabikay <szabi.kay@gmail.com>
2021-04-13 21:35:05 +0200

    Linked list

--- TreeRecursion.py
+++ TreeRecursion.py
@@ -1,9 +1,8 @@
 def calculate(n):
     if n > 0:
-        calculate(n - 1)
+        calculate(n - 5)
         k = n ** 2
         print(k)
-        calculate(n - 1)
+        calculate(n - 5)
 
-
-calculate(2)
+calculate(15)
